ed1296945b479979c30be562f2e29b37bf3ef772
eddie484 <eddieeddie484@gmail.com>
2026-03-18 20:32:42 +0900

    execute.v: nzcv 체크에서 cond와 비교하던 치명적 오류를 수정.
    이제 case로 각각의 경우에 맞도록 체크한다.

--- G.I.S.A._CPU/five_stage/execute.v
+++ G.I.S.A._CPU/five_stage/execute.v
@@ -1,3 +1,5 @@
+`include "v/defines.v"
+
 module execute (clk, nRESET, icode_in, load_in, dmen_in, dmrw_in, aluop_in, sign_in, mulsel_in, lk_in, valA_in, valB_in, wdata_in, dmsize_in, dmsext_in, setcc_in, cond_in, branch_in, pcplus4_in, rD_in, wben_in, icode_out, load_out, dmen_out, dmrw_out, lk_out, valE_out, wdata_out, dmsize_out, dmsext_out, pcplus4_out, rD_out, wben_out, taken_out);
 
 	input clk, nRESET;
@@ -11,16 +13,15 @@ module execute (clk, nRESET, icode_in, load_in, dmen_in, dmrw_in, aluop_in, sign
 	output [5:0] icode_out;
 	output [3:0] rD_out;
 	output [1:0] dmsize_out;
-	output load_out, dmen_out, dmrw_out, lk_out, dmsext_out, wben_out, taken_out;
+	output load_out, dmen_out, dmrw_out, lk_out, dmsext_out, wben_out;
+	output reg taken_out;
 	
 	wire [3:0] alucc, nzcv;
 	
 	
 	ALU ALU_module (valA_in, valB_in, sign_in, aluop_in, mulsel_in, valE_out, alucc);
 	nzcv_reg nzcv_module (clk, nRESET, alucc, setcc_in, nzcv);
-	
-	assign taken_out = ((nzcv == cond_in) && branch_in) ? 1 : 0;
-	
+		
 	
 	assign icode_out = icode_in;
 	assign load_out = load_in;
@@ -33,5 +34,30 @@ module execute (clk, nRESET, icode_in, load_in, dmen_in, dmrw_in, aluop_in, sign
 	assign pcplus4_out = pcplus4_in;
 	assign rD_out = rD_in;
 	assign wben_out = wben_in;
+	
+	
+	always @(*) begin
+		if (branch_in) begin
+			case (cond_in)
+				`EQ: taken_out = (nzcv[2] == 1'b1);
+				`NE: taken_out = (nzcv[2] == 1'b0);
+				`CS, `HS: taken_out = (nzcv[1] == 1'b1);
+				`CC, `LO: taken_out = (nzcv[1] == 1'b0);
+				`MI: taken_out = (nzcv[3] == 1'b1);
+				`PL: taken_out = (nzcv[3] == 1'b0);
+				`VS: taken_out = (nzcv[0] == 1'b1);
+				`VC: taken_out = (nzcv[0] == 1'b0);
+				`HI: taken_out = ((nzcv[1] == 1'b1) && (nzcv[2] == 1'b0));
+				`LS: taken_out = ((nzcv[1] == 1'b0) || (nzcv[2] == 1'b1));
+				`GE: taken_out = (nzcv[3] == nzcv[0]);
+				`LT: taken_out = (nzcv[3] != nzcv[0]);
+				`GT: taken_out = ((nzcv[2] == 1'b0) && (nzcv[3] == nzcv[0]));
+				`LE: taken_out = ((nzcv[2] == 1'b1) || (nzcv[3] != nzcv[0]));
+				`AL: taken_out = 1'b1;
+				default: taken_out = 1'b0;
+			endcase		
+		end else
+			taken_out = 1'b0;
+	end
 
 endmodule
