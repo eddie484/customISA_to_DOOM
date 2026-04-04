4c05e8801e91fc5fa515690869493a5c019980fb
eddie484 <eddieeddie484@gmail.com>
2026-04-04 21:08:16 +0900

    dmem을 ip를 이용하도록 수정.
    기존: reg 배열
    신규: ip 이용

--- G.I.S.A.-CPU/memory/dmem.v
+++ G.I.S.A.-CPU/memory/dmem.v
@@ -1,4 +1,4 @@
-module dmem (clk, nRESET, valE_in, dmen_in, dmrw_in, dmsize_in, dmsext_in, wdata_in, valM_out);
+(* keep_hierarchy *) module dmem (clk, nRESET, valE_in, dmen_in, dmrw_in, dmsize_in, dmsext_in, wdata_in, valM_out);
 
 	input clk, nRESET;
 	input [31:0] valE_in, wdata_in;
@@ -6,53 +6,111 @@ module dmem (clk, nRESET, valE_in, dmen_in, dmrw_in, dmsize_in, dmsext_in, wdata
 	input [1:0] dmsize_in;
 	input dmsext_in;
 	
-	output reg [31:0] valM_out;
+	(* keep *) output reg [31:0] valM_out;
 	
-	reg [7:0] dmem [0:2047];
+	wire [31:0] rdata;
+
+	reg [31:0] wdata;
+	reg [10:0] addr;
+	reg [3:0] byte_enabled;
+	reg [1:0] write_location;
 	
-	integer i;
+	reg [1:0] read_size, read_location;
+	reg read_sext;
 	
-	
-	always @(posedge clk) begin
-		if (!nRESET) begin
-			for (i = 0; i < 2048; i = i + 1) begin
-				dmem[i] <= 8'b0;
-			end
-		end else if (dmen_in) begin
-			if (dmrw_in) begin	// write
-				case (dmsize_in)
-					2'b00: begin
-						dmem[valE_in + 3] <= wdata_in[31:24];
-						dmem[valE_in + 2] <= wdata_in[23:16];
-						dmem[valE_in + 1] <= wdata_in[15:8];
-						dmem[valE_in] <= wdata_in[7:0];
-					end
-					2'b01: begin
-						dmem[valE_in] <= wdata_in[7:0];
-					end
-					2'b10: begin
-						dmem[valE_in + 1] <= wdata_in[15:8];
-						dmem[valE_in] <= wdata_in[7:0];
-					end
-					default: ;
-				endcase		
-			end
-		end
-	end
+	dmem_ram_1port dmem_ram_1port_module (.data(wdata), .address(addr), .wren(dmrw_in), .clock(clk), .clken(dmen_in), .rden(!dmrw_in), .byteena(byte_enabled), .q(rdata));
 	
 	
 	always @(*) begin
+		wdata = 32'b0;
+		addr = 11'b0;
+		byte_enabled = 4'b1111;
+		write_location = 2'b0;
 		valM_out = 32'b0;
+		
 		if (dmen_in) begin
-			if (!dmrw_in)begin		// read
+			addr = valE_in[12:2];
+			write_location = valE_in[1:0];
+			if (dmrw_in) begin	// write
 				case (dmsize_in)
-					2'b00: valM_out = {dmem[valE_in + 3], dmem[valE_in + 2], dmem[valE_in + 1], dmem[valE_in]};
-					2'b01: valM_out = dmsext_in ? {{24{dmem[valE_in][7]}}, dmem[valE_in]} : {24'b0, dmem[valE_in]};
-					2'b10: valM_out = dmsext_in ? {{16{dmem[valE_in + 1][7]}}, dmem[valE_in + 1], dmem[valE_in]} : {16'b0, dmem[valE_in + 1], dmem[valE_in]};
-					default: valM_out = 32'b0;
+					2'b00: begin
+						byte_enabled = 4'b1111;
+						wdata = wdata_in;
+					end
+					
+					2'b01: begin
+						case (write_location)
+							2'b00: begin
+									byte_enabled = 4'b0001;
+									wdata = wdata_in;
+								end
+							2'b01: begin
+									byte_enabled = 4'b0010;
+									wdata = wdata_in << 4'b1000;
+								end
+							2'b10: begin
+									byte_enabled = 4'b0100;
+									wdata = wdata_in << 5'b10000;
+								end
+							2'b11: begin
+									byte_enabled = 4'b1000;
+									wdata = wdata_in << 5'b11000;
+								end
+							default: ;
+						endcase
+					end
+					
+					2'b10: begin
+						case (write_location)
+							2'b00: begin
+								byte_enabled = 4'b0011;
+								wdata = wdata_in;
+							end
+							2'b10: begin
+								byte_enabled = 4'b1100;
+								wdata = wdata_in << 5'b10000;
+							end
+							default: ;
+						endcase
+					end
+					
+					default: ;
+				endcase		
+			end 		
+		end
+		
+		case (read_size) // read
+			2'b00: valM_out = rdata;
+			2'b01: begin
+				case (read_location)
+					2'b00: valM_out = read_sext ? {{24{rdata[7]}}, rdata[7:0]} : {24'b0, rdata[7:0]};
+					2'b01: valM_out = read_sext ? {{24{rdata[15]}}, rdata[15:8]} : {24'b0, rdata[15:8]};
+					2'b10: valM_out = read_sext ? {{24{rdata[23]}}, rdata[23:16]} : {24'b0, rdata[23:16]};
+					2'b11: valM_out = read_sext ? {{24{rdata[31]}}, rdata[31:24]} : {24'b0, rdata[31:24]};
+					default: ;
 				endcase
 			end
+			2'b10: begin
+				case (read_location)
+					2'b00: valM_out = read_sext ? {{16{rdata[15]}}, rdata[15:0]} : {16'b0, rdata[15:0]};
+					2'b10: valM_out = read_sext ? {{16{rdata[31]}}, rdata[31:16]} : {16'b0, rdata[31:16]};
+					default: ;
+				endcase
+			end
+			default: valM_out = 32'b0;
+		endcase	
+	end
+	
+	always @(posedge clk) begin
+		if (!nRESET) begin
+			read_size <= 2'b0;
+			read_location <= 2'b0;
+			read_sext <= 1'b0;
+		end else if (dmen_in && !dmrw_in) begin
+			read_size <= dmsize_in;
+			read_location <= valE_in[1:0];
+			read_sext <= dmsext_in;
 		end
 	end
-
+	
 endmodule
--- G.I.S.A.-CPU/pipeline.v
+++ G.I.S.A.-CPU/pipeline.v
@@ -1,14 +1,14 @@
 `include "defines.v"
 
 
-module pipeline_controller (icode_D, icode_E, rA_D, rB_D, rD_E, mispred_E, stall_F, stall_D, bubble_D, bubble_E);
+(* keep_hierarchy *) module pipeline_controller (icode_D, icode_E, rA_D, rB_D, rD_E, mispred_E, stall_F, stall_D, bubble_D, bubble_E);
 
 	input [5:0] icode_D, icode_E;
 	input [3:0] rA_D, rB_D, rD_E;
 	input mispred_E;
 	
-	output stall_F, stall_D;
-	output bubble_D, bubble_E;
+	(* keep *) output stall_F, stall_D;
+	(* keep *) output bubble_D, bubble_E;
 	
 	
 	assign stall_F = ((icode_E == `LDR || icode_E == `LDRB || icode_E == `LDRSB || icode_E == `LDRH || icode_E == `LDRSH) && (rD_E == rA_D || rD_E == rB_D));
@@ -20,12 +20,12 @@ endmodule
 
 
 
-module pipereg_F (clk, nRESET, stall_F, nextPC, PC_F);
+(* keep_hierarchy *) module pipereg_F (clk, nRESET, stall_F, nextPC, PC_F);
 
 	input clk, nRESET, stall_F;
 	input [31:0] nextPC;
 	
-	output reg [31:0] PC_F;
+	(* keep *) output reg [31:0] PC_F;
 	
 	
 	always @(posedge clk)
@@ -38,30 +38,31 @@ endmodule
 
 
 
-module pipereg_D (clk, nRESET, stall_D, bubble_D, PC_F, IR_F, PCplus4_F, PC_D, IR_D, PCplus4_D);
+(* keep_hierarchy *) module pipereg_D (clk, nRESET, stall_D, bubble_D, PC_F, IR_F, PCplus4_F, PC_D, IR_D, PCplus4_D);
 
 	input clk, nRESET, stall_D, bubble_D;
 	input [31:0] PC_F, IR_F, PCplus4_F;
 	
-	output reg [31:0] PC_D, IR_D, PCplus4_D;
+	(* keep *) output reg [31:0] PC_D, PCplus4_D;
+	(* keep *) output [31:0] IR_D;
 	
 	
 	always @(posedge clk)
 		if (!nRESET || bubble_D) begin
 			PC_D <= 32'b0;
-			IR_D <= 32'b0;
 			PCplus4_D <= 32'b0;
 		end else if (!stall_D) begin
 			PC_D <= PC_F;
-			IR_D <= IR_F;
 			PCplus4_D <= PCplus4_F;
 		end
+		
+	assign IR_D = IR_F;
 		
 endmodule
 
 
 
-module pipereg_E (clk, nRESET, bubble_E, icode_D, load_D, dmen_D, dmrw_D, aluop_D, sign_D, mulsel_D, lk_D, valA_D, valB_D, wdata_D, dmsize_D, dmsext_D, setcc_D, cond_D, branch_D, PCplus4_D, rD_D, wben_D, icode_E, load_E, dmen_E, dmrw_E, aluop_E, sign_E, mulsel_E, lk_E, valA_E, valB_E, wdata_E, dmsize_E, dmsext_E, setcc_E, cond_E, branch_E, PCplus4_E, rD_E, wben_E);
+(* keep_hierarchy *) module pipereg_E (clk, nRESET, bubble_E, icode_D, load_D, dmen_D, dmrw_D, aluop_D, sign_D, mulsel_D, lk_D, valA_D, valB_D, wdata_D, dmsize_D, dmsext_D, setcc_D, cond_D, branch_D, PCplus4_D, rD_D, wben_D, icode_E, load_E, dmen_E, dmrw_E, aluop_E, sign_E, mulsel_E, lk_E, valA_E, valB_E, wdata_E, dmsize_E, dmsext_E, setcc_E, cond_E, branch_E, PCplus4_E, rD_E, wben_E);
 
 	input clk, nRESET, bubble_E;
 
@@ -71,11 +72,11 @@ module pipereg_E (clk, nRESET, bubble_E, icode_D, load_D, dmen_D, dmrw_D, aluop_
 	input [1:0] mulsel_D, dmsize_D;
 	input load_D, dmen_D, dmrw_D, sign_D, lk_D, dmsext_D, setcc_D, branch_D, wben_D;
 
-	output reg [31:0] valA_E, valB_E, wdata_E, PCplus4_E;
-	output reg [5:0] icode_E;
-	output reg [3:0] aluop_E, cond_E, rD_E;
-	output reg [1:0] mulsel_E, dmsize_E;
-	output reg load_E, dmen_E, dmrw_E, sign_E, lk_E, dmsext_E, setcc_E, branch_E, wben_E;
+	(* keep *) output reg [31:0] valA_E, valB_E, wdata_E, PCplus4_E;
+	(* keep *) output reg [5:0] icode_E;
+	(* keep *) output reg [3:0] aluop_E, cond_E, rD_E;
+	(* keep *) output reg [1:0] mulsel_E, dmsize_E;
+	(* keep *) output reg load_E, dmen_E, dmrw_E, sign_E, lk_E, dmsext_E, setcc_E, branch_E, wben_E;
 
 	
 	always @(posedge clk)
@@ -125,7 +126,7 @@ endmodule
 
 
 
-module pipereg_M (clk, nRESET, icode_E, load_E, dmen_E, dmrw_E, lk_E, valE_E, wdata_E, dmsize_E, dmsext_E, PCplus4_E, rD_E, wben_E, icode_M, load_M, dmen_M, dmrw_M, lk_M, valE_M, wdata_M, dmsize_M, dmsext_M, PCplus4_M, rD_M, wben_M);
+(* keep_hierarchy *) module pipereg_M (clk, nRESET, icode_E, load_E, dmen_E, dmrw_E, lk_E, valE_E, wdata_E, dmsize_E, dmsext_E, PCplus4_E, rD_E, wben_E, icode_M, load_M, dmen_M, dmrw_M, lk_M, valE_M, wdata_M, dmsize_M, dmsext_M, PCplus4_M, rD_M, wben_M);
 
 	input clk, nRESET;
 	
@@ -135,11 +136,11 @@ module pipereg_M (clk, nRESET, icode_E, load_E, dmen_E, dmrw_E, lk_E, valE_E, wd
 	input [1:0] dmsize_E;
 	input load_E, dmen_E, dmrw_E, lk_E, dmsext_E, wben_E;
 	
-	output reg [31:0] valE_M, wdata_M, PCplus4_M;
-	output reg [5:0] icode_M;
-	output reg [3:0] rD_M;
-	output reg [1:0] dmsize_M;
-	output reg load_M, dmen_M, dmrw_M, lk_M, dmsext_M, wben_M;
+	(* keep *) output reg [31:0] valE_M, wdata_M, PCplus4_M;
+	(* keep *) output reg [5:0] icode_M;
+	(* keep *) output reg [3:0] rD_M;
+	(* keep *) output reg [1:0] dmsize_M;
+	(* keep *) output reg load_M, dmen_M, dmrw_M, lk_M, dmsext_M, wben_M;
 
 	
 	always @(posedge clk)
@@ -175,7 +176,7 @@ endmodule
 
 
 
-module pipereg_W (clk, nRESET, icode_M, load_M, lk_M, valE_M, valM_M, PCplus4_M, rD_M, wben_M, icode_W, load_W, lk_W, valE_W, valM_W, PCplus4_W, rD_W, wben_W);
+(* keep_hierarchy *) module pipereg_W (clk, nRESET, icode_M, load_M, lk_M, valE_M, valM_M, PCplus4_M, rD_M, wben_M, icode_W, load_W, lk_W, valE_W, valM_W, PCplus4_W, rD_W, wben_W);
 
 	input clk, nRESET;
 	
@@ -184,16 +185,17 @@ module pipereg_W (clk, nRESET, icode_M, load_M, lk_M, valE_M, valM_M, PCplus4_M,
 	input [3:0] rD_M;
 	input load_M, lk_M, wben_M;
 	
-	output reg [31:0] valE_W, valM_W, PCplus4_W;
-	output reg [5:0] icode_W;
-	output reg [3:0] rD_W;
-	output reg load_W, lk_W, wben_W;
+	(* keep *) output [31:0] valM_W;
+	
+	(* keep *) output reg [31:0] valE_W, PCplus4_W;
+	(* keep *) output reg [5:0] icode_W;
+	(* keep *) output reg [3:0] rD_W;
+	(* keep *) output reg load_W, lk_W, wben_W;
 
 	
 	always @(posedge clk)
 		if (!nRESET) begin
 			valE_W <= 32'b0;
-			valM_W <= 32'b0;
 			PCplus4_W <= 32'b0;
 			icode_W <= 6'b0;
 			rD_W <= 4'b0;
@@ -202,7 +204,6 @@ module pipereg_W (clk, nRESET, icode_M, load_M, lk_M, valE_M, valM_M, PCplus4_M,
 			wben_W <= 1'b0;
 		end else begin
 			valE_W <= valE_M;
-			valM_W <= valM_M;
 			PCplus4_W <= PCplus4_M;
 			icode_W <= icode_M;
 			rD_W <= rD_M;
@@ -210,5 +211,7 @@ module pipereg_W (clk, nRESET, icode_M, load_M, lk_M, valE_M, valM_M, PCplus4_M,
 			lk_W <= lk_M;
 			wben_W <= wben_M;
 		end
+		
+	assign valM_W = valM_M;
 		
 endmodule
