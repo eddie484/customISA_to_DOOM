f583f30b74619ae82e0dc67df8c16737febe2962
eddie484 <eddieeddie484@gmail.com>
2026-04-09 14:11:48 +0900

    gisa_assembler.c 주석 추가 및 ,제거 개선

--- G.I.S.A.-Assembler/gisa_assembler.c
+++ G.I.S.A.-Assembler/gisa_assembler.c
@@ -33,58 +33,70 @@ uint32_t labelget(char *labelname){
     exit(1);
 }
 
-// b, jmp에서 라벨 받을 수 있도록 해야 함. 아직 라벨처리 미지원.
+// b, jmp의 단거리 라벨 지원. 아직 장거리 라벨처리 미지원.
+
+
+/* *******************************
+**********************************
+**********************************
+***********            ***********
+***********    MAIN    ***********
+***********            ***********
+**********************************
+**********************************
+******************************* */
 
 int main()
 {
-    FILE *asmfp = fopen("assembly.asm", "r");
-    FILE *binfp = fopen("binary.bin", "w");
+    FILE *asmfp = fopen("assembly.asm", "r");       // 처리할 어셈블리가 적힌 파일 오픈
+    FILE *binfp = fopen("binary.bin", "w");         // 처리 결과 바이너리를 저장할 파일 오픈
 
-    if (asmfp == NULL || binfp == NULL) {
+    if (asmfp == NULL || binfp == NULL) {           // 두 파일 중 하나라도 열지 못할 시 비정상 종료
         printf("파일 읽기 실패\n");
         return 1;
     }
 
 
-    char buf[100];
-    char binbuf[40];
-    char **lines = NULL;
-    char **original_lines = NULL;
-    uint32_t count = 0;
-    int cap = 10;
+    char buf[100];          // 읽어온 어셈블리 코드 라인을 저장. 
+    char binbuf[40];        // 변환된 바이너리 코드를 이진수로 저장. 가독성을 위해 4자리마다 _를 표기한다.
+    char **lines = NULL;            // 명령 어셈블리 라인을 저장. 이 문자열 배열을 쪼개 어셈블리를 바이너리로 번역한다.
+    char **original_lines = NULL;   // 명령 어셈블리의 원본을 보존. 번역 후에도 손상되지 않은 어셈블리가 들어있다.
+    uint32_t count = 0;     // 명령 어셈블리의 라인 수를 계산한다.
+    int cap = 10;           // 문자열 배열의 크기를 의미한다.
 
-    lines = malloc(cap * sizeof(char *));
+    lines = malloc(cap * sizeof(char *));               // 라인 수를 cap만큼으로, 문자열 배열 lines을 malloc한다.
     original_lines = malloc(cap * sizeof(char *));
 
-    while (fgets(buf, sizeof(buf), asmfp)) {
+    while (fgets(buf, sizeof(buf), asmfp)) {        // 입력으로 받은 어셈블리 파일을 한 줄씩 읽어온다. 끝까지 읽어올 때까지 루프한다.
 
-        char *findcomment = strchr(buf, ';');
-        if (findcomment != NULL){
-            findcomment[0] = '\n';
-            findcomment[1] = '\0';
+        char *findcomment = strchr(buf, ';');       // buf에서 ;를 찾아 이후 내용을 제거한다. 주석 처리를 담당.
+        if (findcomment != NULL){       // ;이 있다면 ;를 줄바꿈으로, ; 다음 자리를 null로 바꾼다.
+            findcomment[0] = '\n';      
+            findcomment[1] = '\0';      
         }
 
-        char *findcomma = strchr(buf, ',');
-        if (findcomma != NULL){
-           * findcomma = ' ';
+        char *findcomma = strchr(buf, ',');     // buf에서 ,를 찾아 space로 바꾼다. asm의 인자 구분자의 통일을 담당.
+        while (findcomma != NULL){         // ,이 있다면 space로 바꾼다.
+            *findcomma = ' ';              
+            findcomma = strchr(buf, ',');     // ,이 더 있는지 확인한다. 있다면 반복, 없다면 탈출.
         }
 
-        char *islabel = strchr(buf, ':');
+        char *islabel = strchr(buf, ':');       // buf에서 :를 찾는다. :이 존재한다면 라벨 라인이다.
 
-        if (islabel != NULL) {
-            printf("라벨 라인! %d줄!\n", count);
-            *islabel = '\0';
-            labelput(buf, count);
-            continue;
-        } else if (buf[0] == '\n') {
-            printf("빈 라인! %d줄!\n", count);
-            continue;
+        if (islabel != NULL) {          // 라벨 라인이라면 해당 라인을 비우고 labelput을 호출해, 라벨명과 라인 위치를 기록한다.
+            printf("라벨 라인! %d줄!\n", count);    
+            *islabel = '\0';                      
+            labelput(buf, count);                 
+            continue;                             // 명령어 라인이 아니므로, 라인수 카운트 증가 및 어셈블리 로딩 없이 다음 라인으로 넘어간다.
+        } else if (buf[0] == '\n') {    // 라벨 라인이 아니며, 라인이 비어있는 경우.
+            printf("빈 라인! %d줄!\n", count);     
+            continue;                             // 명령어 라인이 아니므로, 라인수 카운트 증가 및 어셈블리 로딩 없이 다음 라인으로 넘어간다.
         }
 
-        if (count >= cap) {
-            cap *= 2;
-            lines = realloc(lines, cap * sizeof(char *));
-            original_lines = realloc(original_lines, cap * sizeof(char *));
+        if (count >= cap) {         // 라인 수가 배열 크기 한계에 도달한다면
+            cap *= 2;               // 배열 크기 한계를 두배로 늘린다.
+            lines = realloc(lines, cap * sizeof(char *));   // realloc를 통해 lines 배열 크기를 두배로 늘어난 cap에 맞춘다.
+            original_lines = realloc(original_lines, cap * sizeof(char *));   
         }
 
         /*for (int i = 0; i < sizeof(buf); i++){
@@ -95,22 +107,34 @@ int main()
 
 
 
-        lines[count] = malloc(strlen(buf) + 1);
-        original_lines[count] = malloc(strlen(buf) + 1);
-        strcpy(lines[count], buf);
-        strcpy(original_lines[count], buf);
+        lines[count] = malloc(strlen(buf) + 1);             // lines[count]의 크기를 어셈블리 코드만큼 할당한다.
+        original_lines[count] = malloc(strlen(buf) + 1);    
+        strcpy(lines[count], buf);              // 전처리 완료되어 buf에 들어있는 어셈블리를 lines에 저장한다.
+        strcpy(original_lines[count], buf);     
 
-        printf("%d     %s", count, lines[count]);
-        count++;
+        printf("%d     %s", count, lines[count]);       // 검증을 위해, lines에 저장한 어셈블리를 출력한다.
+        count++;        // 다음 라인 처리를 위해 라인 수를 1 증가시킨다.
     }
 
     printf("\n\n--- 어셈블리 불러오기 완료! ---\n\n\n", count, lines[count]);
 
 
 
-    for (int i = 0; i < count; i++) {
-        printf("line %d\t", i);
-        uint32_t binary = 0;
+
+
+/* *******************************
+**********************************
+**********************************
+*********                *********
+*********    ASSEMBLE    *********
+*********                *********
+**********************************
+**********************************
+******************************* */
+
+    for (int i = 0; i < count; i++) {       // 모든 명령 어셈블리 라인을 처리할 때까지 루프.
+        printf("line %d\t", i);             // 몇번째 라인을 처리중인지 출력
+        uint32_t binary = 0;                // 번역된 바이너리 코드가 담길, 32비트 부호없는 정수 변수 선언.
         char *token = strtok(lines[i], " \t\n");    // 첫 번째 토큰으로 opcode를 확인한다.
 
         if (strcmp(token, "MOV") == 0) {            // opcode가 MOV일 경우
@@ -118,7 +142,7 @@ int main()
             int rD = atoi(token + 1);               // 목적지 레지스터의 번호를 int로 변환
             binary |= (rD << 20);                   // 바이너리 코드의 rD 자리를 채운다.
             
-            token = strtok(NULL, " \t\n");
+            token = strtok(NULL, " \t\n");          // 세 번째 토큰으로 소스 레지스터 B를 확인한다.
             int rB = atoi(token + 1);
             binary |= (rB << 12);
 
@@ -129,9 +153,9 @@ int main()
             int rD = atoi(token + 1);
             binary |= (rD << 20);
             
-            token = strtok(NULL, " \t\n");
+            token = strtok(NULL, " \t\n");          // 세 번째 토큰으로 immB를 확인한다.
             int immB = atoi(token + 1);
-            binary |= (immB & 0x000fffffU); // <- 나머지에도 이거 해서 즉시값 부호가 범위 추가하지 않도록 고쳐야 함!!
+            binary |= (immB & 0x000fffffU);         // 즉시값 부호가 범위를 초과하지 않도록 제한한다.
 
         } else if (strcmp(token, "MOVIZ") == 0) {
             binary |= (0b00000011U << 24);
@@ -1718,7 +1742,7 @@ int main()
             binary |= (0b110010U << 26);
             
             token = strtok(NULL, " \t\n");
-            if (token[0] != '#') {
+            if (token[0] != '#') {      // 라벨 branch인 경우
                 int labelline = labelget(token);    // 어떤 라벨인지 감지 후, 라벨이 라인 몇을 가리키는지 가져옴
                 int immB = (labelline - i) * 4 ;    // (현재 라인 - 라벨 라인) * 4. 현재 라인은 int i에서 명시중.
                 if(immB < 2097151 && immB > -2097151){    // 결과값이 22비트 안쪽인지 확인 후 immB로 만들고 바이너리 생성
@@ -1729,7 +1753,7 @@ int main()
                     exit(1);
                 }
                 
-            } else {
+            } else {      // 주소지정 branch인 경우
                 int immB = atoi(token + 1);
                 if(immB < 2097151 && immB > -2097151){    // 결과값이 22비트 안쪽인지 확인 후 immB로 만들고 바이너리 생성
                     immB = immB & 0x3fffff;
@@ -2178,7 +2202,7 @@ int main()
             
             char *tokenjmp1 = strtok(NULL, " \t\n");
 
-            if (strcmp(tokenjmp1, "R0") != 0 &&
+            if (strcmp(tokenjmp1, "R0") != 0 &&     // 두 번째 토큰이 레지스터 지칭이 아닐 경우, 라벨 점프로 간주한다.
                 strcmp(tokenjmp1, "R1") != 0 &&
                 strcmp(tokenjmp1, "R2") != 0 &&
                 strcmp(tokenjmp1, "R3") != 0 &&
@@ -2193,7 +2217,23 @@ int main()
                 strcmp(tokenjmp1, "R12") != 0 &&
                 strcmp(tokenjmp1, "R13") != 0 &&
                 strcmp(tokenjmp1, "R14") != 0 &&
-                strcmp(tokenjmp1, "R15") != 0)
+                strcmp(tokenjmp1, "R15") != 0 &&
+                strcmp(tokenjmp1, "r0") != 0 &&
+                strcmp(tokenjmp1, "r1") != 0 &&
+                strcmp(tokenjmp1, "r2") != 0 &&
+                strcmp(tokenjmp1, "r3") != 0 &&
+                strcmp(tokenjmp1, "r4") != 0 &&
+                strcmp(tokenjmp1, "r5") != 0 &&
+                strcmp(tokenjmp1, "r6") != 0 &&
+                strcmp(tokenjmp1, "r7") != 0 &&
+                strcmp(tokenjmp1, "r8") != 0 &&
+                strcmp(tokenjmp1, "r9") != 0 &&
+                strcmp(tokenjmp1, "r10") != 0 &&
+                strcmp(tokenjmp1, "r11") != 0 &&
+                strcmp(tokenjmp1, "r12") != 0 &&
+                strcmp(tokenjmp1, "r13") != 0 &&
+                strcmp(tokenjmp1, "r14") != 0 &&
+                strcmp(tokenjmp1, "r15") != 0)
             {
                 uint32_t labelline = labelget(tokenjmp1);
                 labelline *= 4;
@@ -2205,7 +2245,7 @@ int main()
                 } else {
                     // 24비트 초과 시.
                 }
-            } else {
+            } else {    // 레지스터 지정 점프일 경우
                 int rB = atoi(tokenjmp1 + 1);
                 binary |= (rB << 12);
             }
@@ -2242,7 +2282,23 @@ int main()
                 strcmp(tokenjmp1, "R12") != 0 &&
                 strcmp(tokenjmp1, "R13") != 0 &&
                 strcmp(tokenjmp1, "R14") != 0 &&
-                strcmp(tokenjmp1, "R15") != 0)
+                strcmp(tokenjmp1, "R15") != 0 &&
+                strcmp(tokenjmp1, "r0") != 0 &&
+                strcmp(tokenjmp1, "r1") != 0 &&
+                strcmp(tokenjmp1, "r2") != 0 &&
+                strcmp(tokenjmp1, "r3") != 0 &&
+                strcmp(tokenjmp1, "r4") != 0 &&
+                strcmp(tokenjmp1, "r5") != 0 &&
+                strcmp(tokenjmp1, "r6") != 0 &&
+                strcmp(tokenjmp1, "r7") != 0 &&
+                strcmp(tokenjmp1, "r8") != 0 &&
+                strcmp(tokenjmp1, "r9") != 0 &&
+                strcmp(tokenjmp1, "r10") != 0 &&
+                strcmp(tokenjmp1, "r11") != 0 &&
+                strcmp(tokenjmp1, "r12") != 0 &&
+                strcmp(tokenjmp1, "r13") != 0 &&
+                strcmp(tokenjmp1, "r14") != 0 &&
+                strcmp(tokenjmp1, "r15") != 0)
             {
                 int labelline = labelget(tokenjmp1);
                 labelline *= 4;
@@ -2295,14 +2351,13 @@ int main()
 
     }
 
-    char finish[39] = "1111_1111_1111_1111_1111_1111_1111_1111";
-    fwrite (finish, sizeof(char), 39, binfp); 
 
 
     for (int i = 0; i < count; i++) {
         free(lines[i]);
         free(original_lines[i]);
     }
+
     free(lines);
     free(original_lines);
 
