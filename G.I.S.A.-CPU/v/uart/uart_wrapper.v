a99df23e5fc4b2c43b76218661582f2aefec3027
eddie484 <eddieeddie484@gmail.com>
2026-07-30 11:39:22 +0900

    gisa_uart: in/out 타이밍 문제 수정.
    1. receive sig가 데이터를 cpu에 넘기기 전에 초기화해서 256값이 항상 scan되는 문제: 읽은 뒤, 1사이클 늦게 초기화시켜 해결.->이제 초기화가 한 사이클 뒤이므로 receive sig와 uart_en의 우선순위가 바뀌었다. 연속해서 값이 들어오는 경우, receive와 충돌하는 값은 새로 들어온 값이기 때문.
    
    2. send 문제. 값 보내기 전 다른 명령이 wdata를 덮어써, 제대로된 값을 보내지 못한다-> send_sig 오면 reg에 wdata 넣어두어 해결.

--- G.I.S.A.-CPU/v/uart/uart_wrapper.v
+++ G.I.S.A.-CPU/v/uart/uart_wrapper.v
@@ -8,24 +8,39 @@ module uart_wrapper (clk, nRESET, dmrw_in, dmen_in, valE_in, wdata_in, rdx_in, t
 	input dmrw_in, dmen_in;
 	
 	output tdx_out;
-	output reg [32:0] uart_char_out;
+	output reg [31:0] uart_char_out;
 	
 	wire send_command_sig, receive_command_sig;
 	wire [7:0] uart_receive;
 	wire uart_outen;
 	
+	reg receive_command_delay;
+	reg [7:0] send_data;
+	
 	
 	assign send_command_sig = dmrw_in & dmen_in & (valE_in == `MEMM_UART);
 	assign receive_command_sig = !dmrw_in & dmen_in & (valE_in == `MEMM_UART);
 	
-	uart uart_core (clk, nRESET, rdx_in, wdata_in[7:0], send_command_sig, tdx_out, uart_receive, uart_outen);
+	uart uart_core (clk, nRESET, rdx_in, send_data, send_command_sig, tdx_out, uart_receive, uart_outen);
 	
 	always @(posedge clk or negedge nRESET) begin
-		if (!nRESET) uart_char_out <= 8'b0;		
-		else if (receive_command_sig) begin		// receive check의 우선순위가 uart값보다 높다. 읽었다는 사실이 유실되면 다음 읽기에서 읽은 값인지 새 값인지 구분 불가능해지기 때문.
+		if (!nRESET) begin
 			uart_char_out <= 32'h0000_0100;
-		end else if (uart_outen) begin		
-			uart_char_out <= {24'b0, uart_receive};
+			receive_command_delay <= 1'b0;
+			send_data <= 8'b0;
+		end else begin
+			if (uart_outen) begin		
+				uart_char_out <= {24'b0, uart_receive};
+				receive_command_delay <= receive_command_sig;
+			end else if (receive_command_delay) begin		// receive check는 이제 값을 읽은 다음 사이클에 처리되므로 우선순위가 낮다. 읽은 다음 사이클에 바로 uart가 들어오면, 새 값이 저장되는 것이므로 읽은 값은 사라지기 때문.
+				uart_char_out <= 32'h0000_0100;
+				receive_command_delay <= receive_command_sig;
+			end else receive_command_delay <= receive_command_sig;
+			
+			if (send_command_sig) begin
+				send_data <= wdata_in[7:0];
+			end
+			
 		end
 	end
 
